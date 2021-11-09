142c279dce8707ff69c0a8a7a4c6c332d9bff2cc
nazililham11 <nazil.ilham11@gmail.com>
2021-11-10 03:14:06 +0700

    Penambahan sumber gambar

--- src/App.vue
+++ src/App.vue
@@ -1,78 +1,180 @@
 <template>
-    <div class="container py-4">
-
-        <div class="row my-2 py-2">
-            <div class="col-12">
-                <div class="card p-2">
-                    <div class="d-flex flex-row">
-
-                        <!-- Camera Button -->
-                        <div
-                            @click="toggleCamera"
-                            class="btn m-2"
-                            :class="isLoading ? 'btn-warning' : !isCameraOpen ? 'btn-primary' : 'btn-danger'"
-                        >
-                            <span
-                                v-show="isLoading"
-                                class="spinner-border spinner-border-sm"
-                                role="status"
-                                aria-hidden="true"
-                            ></span>
-                            {{ isLoading ? "Loading Camera" : !isCameraOpen ? "Open Camera" : "Close Camera" }}
-                        </div>
-
-                        <!-- Capture Button -->
-                        <div v-show="isCameraOpen" @click="takePhoto" class="btn btn-success m-2">
-                            Capture
-                        </div>
-
-                    </div>
-                </div>
-            </div>
-        </div>
-
-        <div class="row my-2">
-
-            <!-- Camera -->
-            <div class="col-md-6 col-12">
+    <div class="container-fluid p-2">
+        <div class="row p-2">
+            <!-- Photo -->
+            <div class="col-lg-6 col-12 p-2">
                 <div class="card p-2">
                     <div class="card-body">
-                        <div class="d-flex flex-column align-items-center">
-                            <h5 class="card-title">Camera</h5>
+                        <h5 class="card-title">Object Detection</h5>
+                        <!-- Buttons -->
+                        <div class="d-flex flex-row justify-content-between">
+                            <div
+                                class="btn-group"
+                                role="group"
+                                aria-label="Basic example"
+                            >
+                                <!-- From Camera -->
+                                <div
+                                    @click="selectDetectionFrom('camera')"
+                                    class="btn"
+                                    :class="
+                                        detectFrom != 'camera'
+                                            ? 'btn-outline-primary'
+                                            : 'btn-primary'
+                                    "
+                                >
+                                    Camera
+                                </div>
+
+                                <!-- From File -->
+                                <div
+                                    @click="selectDetectionFrom('file')"
+                                    class="btn"
+                                    :class="
+                                        detectFrom != 'file'
+                                            ? 'btn-outline-primary'
+                                            : 'btn-primary'
+                                    "
+                                >
+                                    File
+                                </div>
+
+                                <!-- From URL -->
+                                <div
+                                    @click="selectDetectionFrom('url')"
+                                    class="btn"
+                                    :class="
+                                        detectFrom != 'url'
+                                            ? 'btn-outline-primary'
+                                            : 'btn-primary'
+                                    "
+                                >
+                                    URL
+                                </div>
+                            </div>
+
+                            <div
+                                class="btn btn-success"
+                                :class="{ disabled: !isImageReady }"
+                                @click="predict"
+                            >
+                                Predict
+                            </div>
+                        </div>
+
+                        <hr />
+
+                        <!-- From URL -->
+                        <div v-show="detectFrom == 'url'">
+                            <div class="d-flex flex-row mb-2">
+                                <input
+                                    type="text"
+                                    class="form-control"
+                                    id="image-url"
+                                    placeholder="Image URL"
+                                    v-model="imageUrl"
+                                />
+                                <div @click="getImage" class="btn btn-primary">
+                                    Get
+                                </div>
+                            </div>
+
+                            <img
+                                v-if="currentImageUrl"
+                                @load="imageLoaded"
+                                :src="currentImageUrl"
+                                alt="image"
+                                class="img-fluid mt-2"
+                            />
+                        </div>
+
+                        <!-- From File -->
+                        <div v-show="detectFrom == 'file'">
+                            <input
+                                class="mb-2"
+                                type="file"
+                                @change="fileChanged"
+                            />
+                            <img
+                                v-if="fileInput"
+                                @load="imageLoaded"
+                                :src="fileInput"
+                                class="img-fluid mt-2"
+                                alt="Image"
+                            />
+                        </div>
+
+                        <!-- From Camera -->
+                        <div
+                            v-show="detectFrom == 'camera'"
+                            class="flex-column justify-content-center"
+                        >
+                            <div class="d-flex flex-row">
+                                <div
+                                    @click="toggleCamera"
+                                    class="btn"
+                                    :class="
+                                        isLoading
+                                            ? 'btn-warning'
+                                            : !isCameraOpen
+                                            ? 'btn-primary'
+                                            : 'btn-danger'
+                                    "
+                                >
+                                    <span
+                                        v-show="isLoading"
+                                        class="spinner-border spinner-border-sm"
+                                        role="status"
+                                        aria-hidden="true"
+                                    ></span>
+                                    {{
+                                        isLoading
+                                            ? "Loading Camera"
+                                            : !isCameraOpen
+                                            ? "Open Camera"
+                                            : "Close Camera"
+                                    }}
+                                </div>
+                                <div
+                                    v-show="!isImageReady"
+                                    @click="takePhoto"
+                                    class="btn btn-success mx-2"
+                                    :class="{ disabled: !isCameraOpen }"
+                                >
+                                    Capture
+                                </div>
+
+                                <div
+                                    v-show="isImageReady"
+                                    @click="resetTakedPhoto"
+                                    class="btn btn-warning mx-2"
+                                >
+                                    Reset
+                                </div>
+                            </div>
 
                             <video
-                                v-show="isCameraOpen"
+                                v-show="isCameraOpen && !isImageReady"
                                 id="camera"
-                                class="m-2"
+                                class="my-2"
                                 ref="camera"
                                 autoPlay
-                                width="500"
-                                height="374"
+                                :width="cameraSize.width"
+                                :height="cameraSize.height"
                             ></video>
-                        </div>
-                    </div>
-                </div>
-            </div>
 
-            <!-- Photo -->
-            <div class="col-md-6 col-12">
-                <div class="card p-2">
-                    <div class="card-body">
-                        <div class="d-flex flex-column align-items-center">
-                            <h5 class="card-title">Photo</h5>
-                            
                             <canvas
-                                v-show="isPhotoTaken"
-                                id="photo-taken"
+                                v-show="isCameraOpen && isImageReady"
+                                id="photoTaken"
+                                class="my-2"
                                 ref="canvas"
-                                width="500"
-                                height="374"
+                                :width="cameraSize.width"
+                                :height="cameraSize.height"
                             ></canvas>
                         </div>
                     </div>
                 </div>
             </div>
-
         </div>
     </div>
 </template>
@@ -84,17 +186,22 @@ export default {
         return {
             isLoading: false,
             isCameraOpen: false,
-            isPhotoTaken: false,
+            isImageReady: false,
+            detectFrom: "camera",
+            cameraSize: { width: 500, height: 374 },
+            imageUrl: "",
+            currentImageUrl: "",
+            fileInput: "",
         };
     },
     methods: {
         toggleCamera() {
             if (this.isCameraOpen) {
-                this.isCameraOpen = false
-                this.stopCameraStream()
+                this.isCameraOpen = false;
+                this.stopCameraStream();
             } else {
-                this.isCameraOpen = true
-                this.createCameraElement()
+                this.isCameraOpen = true;
+                this.createCameraElement();
             }
         },
 
@@ -131,10 +238,54 @@ export default {
         },
 
         takePhoto() {
-            this.isPhotoTaken = true;
             const context = this.$refs.canvas.getContext("2d");
-            context.drawImage(this.$refs.camera, 0, 0, 500, 374);
+            context.drawImage(
+                this.$refs.camera,
+                0,
+                0,
+                this.cameraSize.width,
+                this.cameraSize.height
+            );
+            this.isImageReady = true;
         },
+
+        resetTakedPhoto() {
+            this.isImageReady = false;
+        },
+
+        selectDetectionFrom(selected) {
+            if (selected == this.detectFrom) return;
+
+            if (this.detectFrom == "camera" && this.isCameraOpen) {
+                this.isCameraOpen = false;
+                this.stopCameraStream();
+            }
+
+            this.fileInput = "";
+            this.imageUrl = "";
+            this.currentImageUrl = "";
+            this.isImageReady = false;
+
+            this.detectFrom = selected;
+        },
+
+        getImage() {
+            this.currentImageUrl = this.imageUrl;
+        },
+
+        fileChanged(event) {
+            const file = event.target.files[0];
+            this.fileInput = URL.createObjectURL(file);
+        },
+
+        imageLoaded() {
+            this.isImageReady = true;
+        },
+
+        predict() {
+            // Prediction stuff
+        }
+
     },
 };
 
@@ -142,5 +293,7 @@ export default {
 //
 // Vue Webcam Camera Capture
 // https://codepen.io/ditarahma08/pen/GRRxZLW?editors=1010
-
+//
+// Preview an image before it is uploaded VUEjs
+// https://stackoverflow.com/questions/49106045/preview-an-image-before-it-is-uploaded-vuejs
 </script>
